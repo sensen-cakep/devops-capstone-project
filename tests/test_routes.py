d7fcb9df312dc44e30619c1e036cff6cb97d99f3
sensen-cakep <sensen@singnet.com.sg>
2023-06-24 03:44:13 -0400

    commit update account changes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -142,15 +142,21 @@ class TestAccountService(TestCase):
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
-        # send a self.client.get() request to the BASE_URL
-        # assert that the resp.status_code is status.HTTP_200_OK
-        # get the data from resp.get_json()
-        # assert that the len() of the data is 5 (the number of accounts you created)
-
-    def test_get_account_list(self):
-        """It should Get a list of Accounts"""
-        self._create_accounts(5)
         resp = self.client.get(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
