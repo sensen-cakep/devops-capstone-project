37d81f53202a8a2c0abfdebdd347a0f6024381ab
sensen-cakep <sensen@singnet.com.sg>
2023-06-25 03:09:28 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -24,6 +24,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -194,3 +195,4 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+        
